a264c1bc4bf1c0819fb38cf0e78eafcb6256c967
tomas <toma@sokon.com>
2019-04-16 09:58:54 +0800

    完成编写

---
+++ roles/tablespace_ops/files/get_big_object.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VALUE=`su - oracle -l -c "sqlplus -S /nolog" <<EOF
+set termout on  heading off feedback off pagesize 0 verify off echo off 
+connect /as sysdba 
+@/tmp/bigobject.sql
+exit;
+EOF`
+
+echo $VALUE
